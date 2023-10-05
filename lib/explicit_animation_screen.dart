9dff78bab95a2d23f07d3dbe49fd3c663968fb3a
andypsj@naver.com <andypsj@naver.com>
2023-10-05 19:17:32 +0900

    Explicit Animation
    애니메이션 컨트롤러에 addListener를 달아서 변화를 감지할 때마다 상태를 반영하는 setstate를 적용해도 되지만, 스크린에 다수의 컨트롤러가 존재할 경우 매번 setState를 적용하면 너무 많은 리소스를 사용하게 하여 과부하가 될 수 있음.
    따라서 이런 경우 상태값을 UI에 반영할 때 AnimatedBuilder를 사용하여 builder에서 자동으로 상태값이 변할 때를 감지하여 UI에 반영하는 위젯을 사용한다.

--- lib/explicit_animation_screen.dart
+++ lib/explicit_animation_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
@@ -12,13 +14,36 @@ class _ExplicitAnimationState extends State<ExplicitAnimation>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController = AnimationController(
     vsync: this,
-  );
+    duration: const Duration(
+      seconds: 10,
+    ),
+  )..addListener(() {
+      setState(() {});
+    });
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Ticker((elapsed) => print(elapsed));
+
+    Timer.periodic(
+        const Duration(
+          milliseconds: 500,
+        ), (timer) {
+      print(_animationController.value);
+    });
+  }
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
   }
 
   @override
@@ -27,6 +52,50 @@ class _ExplicitAnimationState extends State<ExplicitAnimation>
       appBar: AppBar(
         title: const Text(
           'Explicit Animation',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return Text(
+                  '${_animationController.value}',
+                  style: const TextStyle(
+                    fontSize: 40,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: const Icon(
+                    Icons.pause_rounded,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _play,
+                  child: const Icon(
+                    Icons.play_arrow_rounded,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: const Icon(
+                    Icons.fast_rewind_rounded,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
