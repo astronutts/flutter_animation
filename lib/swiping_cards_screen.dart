5113f761cd795e3c4cd72b31a04ca17e8f531096
andypsj@naver.com <andypsj@naver.com>
2023-10-09 19:48:01 +0900

    * bound는 이미지를 드래그했을 때 튕겨지고, 원점으로 돌아오는 경계를 정의한 것. 만약  _animationController.value의 절대값이 bound보다 클 경우 : 그 값이 음수이면 음수의 방향으로 애니메이션이 진행하고, 양수이면 양수의 방향으로 애니메이션이 진행.
    * 카드의 각도가 원하는 angle만큼 움직이게 하기 위해 Transform.rotate 적용하여 angle을 아래와 같이 정의
    
    final angle = _rotation.transform(
                  (_animationController.value + size.width / 2) / size.width, > 사실 이 부분 잘 이해가 안감;;
    
    * 뒤에 있는 그린카드가 앞에 레드카드가 정의된 angle에 따라 rotate됨에 따라 scale이 커짐.
    
    final scale = _scale.transform(_animationController.value.abs() / size.width);

--- lib/swiping_cards_screen.dart
+++ lib/swiping_cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardsScreen extends StatefulWidget {
@@ -7,51 +9,114 @@ class SwipingCardsScreen extends StatefulWidget {
   State<SwipingCardsScreen> createState() => _SwipingCardsScreenState();
 }
 
-class _SwipingCardsScreenState extends State<SwipingCardsScreen> {
+class _SwipingCardsScreenState extends State<SwipingCardsScreen>
+    with SingleTickerProviderStateMixin {
+  late final size = MediaQuery.of(context).size;
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      seconds: 5,
+    ),
+    lowerBound: (size.width + 100) * -1,
+    upperBound: (size.width + 100),
+    value: 0.0,
+  );
+
+  late final Tween<double> _rotation = Tween(
+    begin: -15,
+    end: 15,
+  );
+
+  late final Tween<double> _scale = Tween(
+    begin: 0.8,
+    end: 1.0,
+  );
+
   double posX = 0;
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      posX += details.delta.dx;
-    });
-    print(posX);
+    _animationController.value += details.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
-    setState(() {
-      posX = 0;
-    });
+    final bound = size.width - 100;
+    if (_animationController.value.abs() >= bound) {
+      if (_animationController.value.isNegative) {
+        _animationController.animateTo((size.width + 100) * -1);
+      } else {
+        _animationController.animateTo(size.width + 100);
+      }
+    } else {
+      _animationController.animateTo(
+        0,
+        curve: Curves.bounceOut,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Swiping Cards'),
-      ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: GestureDetector(
-              onHorizontalDragUpdate: _onHorizontalDragUpdate,
-              onHorizontalDragEnd: _onHorizontalDragEnd,
-              child: Transform.translate(
-                offset: Offset(posX, 0),
-                child: Material(
-                  elevation: 10,
-                  color: Colors.red.shade100,
-                  child: SizedBox(
-                    width: size.width * 0.8,
-                    height: size.height * 0.5,
+        appBar: AppBar(
+          title: const Text('Swiping Cards'),
+        ),
+        body: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            final angle = _rotation.transform(
+              (_animationController.value + size.width / 2) / size.width,
+            );
+
+            final scale =
+                _scale.transform(_animationController.value.abs() / size.width);
+            return Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Positioned(
+                  top: 100,
+                  child: Transform.scale(
+                    scale: scale,
+                    child: Material(
+                      elevation: 10,
+                      color: Colors.green.shade100,
+                      child: SizedBox(
+                        width: size.width * 0.8,
+                        height: size.height * 0.5,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+                Positioned(
+                  top: 100,
+                  child: GestureDetector(
+                    onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                    onHorizontalDragEnd: _onHorizontalDragEnd,
+                    child: Transform.translate(
+                      offset: Offset(_animationController.value, 0),
+                      child: Transform.rotate(
+                        angle: (angle * pi) / 180,
+                        child: Material(
+                          elevation: 10,
+                          color: Colors.red.shade100,
+                          child: SizedBox(
+                            width: size.width * 0.8,
+                            height: size.height * 0.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
